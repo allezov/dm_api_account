35047373f78dd37f47a03ffc3e8acaa46e8ee9f5
Aleksandr Lezov <al.lezov@yandex.ru>
2023-06-02 17:25:58 +0400

    импорт вложенных папок

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIRES = [
 setup(
     name='dm_api_account',
     version='0.0.2',
-    packages=['dm_api_account'],
+    packages=['dm_api_account', 'dm_api_account/apis', 'dm_api_account/models'],
     url='https://github.com/allezov/dm_api_account.git',
     license='Apache-2.0 license',
     author='lezov',
