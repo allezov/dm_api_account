e90fe2663450ce23f8740fc11831c98318bed7b4
Aleksandr Lezov <al.lezov@yandex.ru>
2023-06-02 16:45:05 +0400

    dm_api_account

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+REQUIRES = [
+    'records',
+    'allure-pytest',
+    'structlog',
+    'requests',
+    'pydantic'
+]
+
+setup(
+    name='dm_api_account',
+    version='0.0.1',
+    packages=['dm_api_account'],
+    url='https://github.com/allezov/dm_api_account.git',
+    license='Apache-2.0 license',
+    author='lezov',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='dm_api_account'
+)
